87a5f798939c5897ecac6aef568f2c7ba67dc26c
Artham Siri <siriartham83@gmail.com>
2025-12-26 08:48:50 +0530

    Add standard input and output with type casting example

---
+++ basics_02std_input_output.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")      # This is standard input function it is used to take the data from the users.
+age = int(input("Enter your age: "))   # By default it takes string data type as input . So, here we are converting the string into integer it is called as type conversion.
+
+print("Hello", name, "your age is",age)
+
